ae3c91cc011fcf40a1a1bb71a7a91f915c4a95ae
Larisa Avtukhova <aln1967@yandex.ru>
2021-10-10 20:36:10 +0300

    Changes files

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -3,31 +3,49 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.Test;
 
+import java.time.DayOfWeek;
 import java.time.Duration;
+import java.time.LocalDate;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Condition.exactText;
+import org.junit.jupiter.api.BeforeEach;
 
 
 public class CardDeliveryTest {
 
+    @BeforeEach
+    void setUp() {
+        Configuration.browser = "chrome";
+    }
+
     @Test
     public void shouldCardDelivery() {
-        open("http://localhost:9999");
-        $("[class='input__control'][type='text']").setValue("Воронеж");
-        $("[class='calendar-input__native-control'][type='date']").setValue("20.10.2021");
-        $("[class='input__control'][name='name']").setValue("Елена Иванова");
-        $("[class='input__control'][type='tel']").setValue("+79501055555");
-        $("[class='checkbox__text'][role='presentation']").click();
-        $(Selectors.byText("Забронировать")).click();
-        $(Selectors.withText("Успешно")).shouldBe (visible, Duration.ofSeconds(20));
+      //  open("http://localhost:9999");
+     //   $("[class='input__control'][type='text']").setValue("Воронеж");
+      //  $("[class='calendar-input__native-control'][type='date']").setValue("20.10.2021");
+       // $("[class='input__control'][name='name']").setValue("Елена Иванова");
+       // $("[class='input__control'][type='tel']").setValue("+79501055555");
+      //  $("[class='checkbox__text'][role='presentation']").click();
+      //  $(Selectors.byText("Забронировать")).click();
+       // $(Selectors.withText("Успешно")).shouldBe (visible, Duration.ofSeconds(15));
 
     }
 
+    @Test
+    public void shouldCardDelivery2() {
+        open("http://localhost:9999");
+        $("[data-test-id='city']").setValue("Воронеж");
+        $("[data-test-id='date']").setValue("20.10.2021");
+        $("[data-test-id='name']").setValue("Елена Иванова");
+        $("[data-test-id='phone']").setValue("+79501055555");
+        $("[data-test-id='agreement']").click();
+        $(Selectors.byText("Забронировать")).click();
+        $(Selectors.withText("Успешно")).shouldBe (visible, Duration.ofSeconds(15));
 
-
+    }
 
 
 }
