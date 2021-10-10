6a2815137d1a0d5fcf38b9857d7288eddc8e2201
Larisa Avtukhova <aln1967@yandex.ru>
2021-10-10 19:27:12 +0300

    Changes files

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -14,13 +14,13 @@ import static com.codeborne.selenide.Condition.exactText;
 public class CardDeliveryTest {
 
     @Test
-    private void shouldCardDelivery() {
+    public void shouldCardDelivery() {
         open("http://localhost:9999");
-        $("[data-test-id=city]").setValue("Воронеж");
-        $("[data-test-id=date]").setValue("20.10.2021");
-        $("[data-test-id=name]").setValue("Елена Иванова");
-        $("[data-test-id=phone]").setValue("+79501055555");
-        $("[data-test-id='agreement]").click();
+        $("[class='input__control'][type='text']").setValue("Воронеж");
+        $("[class='calendar-input__native-control'][type='date']").setValue("20.10.2021");
+        $("[class='input__control'][name='name']").setValue("Елена Иванова");
+        $("[class='input__control'][type='tel']").setValue("+79501055555");
+        $("[class='checkbox__text'][role='presentation']").click();
         $(Selectors.byText("Забронировать")).click();
         $(Selectors.withText("Успешно")).shouldBe (visible, Duration.ofSeconds(20));
 
