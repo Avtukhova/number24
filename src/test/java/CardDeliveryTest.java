0d11d6fd1c4dcbbac86b678a6abdc1410b341fdd
Larisa Avtukhova <aln1967@yandex.ru>
2021-10-11 18:30:48 +0300

    Changes test

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -6,28 +6,33 @@ import org.junit.jupiter.api.Test;
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Condition.exactText;
+
 import org.junit.jupiter.api.BeforeEach;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
-
 public class CardDeliveryTest {
 
-     @Test
+    @Test
     public void shouldCardDelivery() {
-       open("http://localhost:9999");
-       $("[class='input__control'][type='text']").setValue("Воронеж");
-       $("[class='calendar-input__native-control'][type='date']").setValue("20.10.2021");
-       $("[class='input__control'][name='name']").setValue("Елена Иванова");
-       $("[class='input__control'][type='tel']").setValue("+79501055555");
-       $("[class='checkbox__text'][role='presentation']").click();
-       $(Selectors.byText("Забронировать")).click();
-       $(Selectors.withText("Успешно")).shouldBe (visible, Duration.ofSeconds(15));
+        open("http://localhost:9999");
+        $("[class='input__control'][type='text']").setValue("Воронеж");
+        LocalDate sourceDate = LocalDate.now();
+        LocalDate desiredDate = sourceDate.plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = desiredDate.format(formatter);
+        $("[class='calendar-input__native-control'][type='date']").setValue("14.10.2021");
+        $("[class='input__control'][name='name']").setValue("Елена Иванова");
+        $("[class='input__control'][type='tel']").setValue("+79501055555");
+        $("[class='checkbox__text'][role='presentation']").click();
+        $(Selectors.byText("Забронировать")).click();
+        $(Selectors.withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
 
     }
 
@@ -40,7 +45,7 @@ public class CardDeliveryTest {
         $("[data-test-id='phone']").setValue("+79501055555");
         $("[data-test-id='agreement']").click();
         $(Selectors.byText("Забронировать")).click();
-        $(Selectors.withText("Успешно")).shouldBe (visible, Duration.ofSeconds(15));
+        $(Selectors.withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
 
     }
 
