c3d3258f1d9e2b0dd6b7a906f433d8b28084bcbb
Larisa Avtukhova <aln1967@yandex.ru>
2021-10-12 21:44:26 +0300

    Changes files

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,4 +1,5 @@
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import static com.codeborne.selenide.Condition.exactText;
 
 import org.junit.jupiter.api.BeforeEach;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Keys;
 
 
 public class CardDeliveryTest {
@@ -23,31 +25,22 @@ public class CardDeliveryTest {
     public void shouldCardDelivery() {
         open("http://localhost:9999");
         $("[class='input__control'][type='text']").setValue("Воронеж");
-        LocalDate sourceDate = LocalDate.now();
-        LocalDate desiredDate = sourceDate.plusDays(3);
+        LocalDate today = LocalDate.now();
+        LocalDate desiredDate = today.plusDays(3);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        String formattedDate = desiredDate.format(formatter);
-        $("[class='calendar-input__native-control'][type='date']").setValue("14.10.2021");
+        String appointmentDate = desiredDate.format(formatter);
+        $("[data-test-id=date] input").doubleClick();
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(appointmentDate);
         $("[class='input__control'][name='name']").setValue("Елена Иванова");
-        $("[class='input__control'][type='tel']").setValue("+79501055555");
+        $("[class='input__control'][name='phone']").setValue("+79501055555");
         $("[class='checkbox__text'][role='presentation']").click();
         $(Selectors.byText("Забронировать")).click();
         $(Selectors.withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
 
     }
 
-    @Test
-    public void shouldCardDelivery2() {
-        open("http://localhost:9999");
-        $("[data-test-id='city']").setValue("Воронеж");
-        $("[data-test-id='date']").setValue("20.10.2021");
-        $("[data-test-id='name']").setValue("Елена Иванова");
-        $("[data-test-id='phone']").setValue("+79501055555");
-        $("[data-test-id='agreement']").click();
-        $(Selectors.byText("Забронировать")).click();
-        $(Selectors.withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
 
-    }
 
 
 }
